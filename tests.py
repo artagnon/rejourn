e108a742e2181e7cd2054d1a9e9adc882f2a1c62
Ramkumar Ramachandra <artagnon@gmail.com>
2010-04-21 15:58:03 +0530

    Update tests
    Update all the test in accordance with the changes in core and util.

--- tests.py
+++ tests.py
@@ -5,23 +5,33 @@ from markdown import markdown
 from core import JEntry
 import util
 
+testfile = '/home/artagnon/dev/rejourn/in/hackers-dream-journal.txt'
+
 class TestAll:
     def __init__(self):
-        self.jentry = JEntry('/home/artagnon/dev/rejourn/in/test.txt')
+        self.jentry = JEntry(testfile)
 
     def test_init(self):
         title = """The hacker's dream journal engine"""
         assert self.jentry.context['title'] == title
-        assert self.jentry.context['permalink'] == util.slugify(title)
+        assert self.jentry.context['permalink'] == util.build_slug(title)
+
+    def test_parse_header(self):
+        raw_header = "view: single\npermalink: the-hacker-s-dream-journal-eng\npublished: True\npubdate: 2010-04-21T12:04:36Z\ntitle: The hacker's dream journal engine\nauthor: Ramkumar Ramachandra"
+        header = util.parse_header(raw_header)
+        assert header['pubdate'] == '2010-04-21T12:04:36Z'
 
     def test_parse_config(self):
-        store = self.jentry._JEntry__parse_config()
-        assert store.get('basedir', None) == "/home/artagnon/dev/rejourn"
+        config = util.parse_config('core.cfg')
+        assert config.get('basedir', None) == "/home/artagnon/dev/rejourn"
 
     def test_publish(self):
         self.jentry.publish()
-        with open('/home/artagnon/dev/rejourn/in/test.txt') as infile:
+        with open(testfile) as infile:
             assert infile.read().find("published: True") != -1
+
+    def test_mtime_check(self):
+        assert self.jentry.publish() == -1
 
     def test_markdown_lib(self):
         assert markdown("*strong* hammer") == "<p><em>strong</em> hammer</p>"
